cae7fa2f665b9e0320b3b91e233f8a980a010515
narah10 <kwa18001@byui.edu>
2023-09-23 17:01:05 -0600

    Added route to the home

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', (req, res)=> {
+    res.send('Routes')
+})
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
