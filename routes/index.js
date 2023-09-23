1b5afcb0fe7c8fe73fc61fa6985f661985bb4ada
narah10 <kwa18001@byui.edu>
2023-09-23 17:13:04 -0600

    Changed the placement route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.use('/contacts', require('./contacts'))
 router.use('/', (req, res)=> {
     res.send('Routes')
 })
-router.use('/contacts', require('./contacts'))
 
 module.exports = router;
