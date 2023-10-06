46be3be79786812b48ec945176f688dcb83e7460
narah10 <kwa18001@byui.edu>
2023-10-06 11:23:15 -0600

    Added cors

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require('express')
-// var cors = require('cors');
+const express = require('express');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const routes = require('./routes/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
+  .use(cors())
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
