57a94973523209cef54ae5bf66b620dbec40cb87
narah10 <kwa18001@byui.edu>
2023-10-07 18:29:48 -0600

    changed to https and removed the beginning url

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'https://cse341-first-apis.onrender.com',
+  host: 'cse341-first-apis.onrender.com',
   schemes: ['https'],
 };
 
